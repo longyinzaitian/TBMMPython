0767e74e038f8b985a21429589d9f3af6b3046e9
longyinzaitian <husyin@189.cn>
2015-11-22 12:50:45 +0800

    淘宝淘女郎信息抓取

--- scr/spider.py
+++ scr/spider.py
@@ -21,29 +21,34 @@ class Spider:
     #获取索引界面所有MM的信息，list格式
     def getContents(self,pageIndex):
         page = self.getPage(pageIndex)
-        pattern = re.compile('<div class="list-item".*?pic-word.*?<a href="(.*?)".*?<img src="(.*?)".*?<a class="lady-name.*?>(.*?)</a>.*?<strong>(.*?)</strong>.*?<span>(.*?)</span>',re.S)
+        # 这里获取六条信息 个人信息网址，头像图片地址，美眉个人信息页地址，美眉名字，年龄，居住地址
+        pattern = re.compile('<div class="list-item".*?pic-word.*?<a href="(.*?)".target=.*?<img src="(.*?)".alt.*?<a class="lady-name".href=(.*?).target=.*?>(.*?)</a>.*?<strong>(.*?)</strong>.*?<span>(.*?)</span>',re.S)
         items = re.findall(pattern,page)
         contents = []
         for item in items:
-            contents.append([item[0],item[1],item[2],item[3],item[4]])
+            contents.append([item[0],item[1],item[2],item[3],item[4],item[5]])
         return contents
     #获取MM个人详情页面
     def getDetailPage(self,infoURL):
-        response = urllib2.urlopen("http:"+infoURL)
+        response = urllib2.urlopen("https:"+infoURL)
         return response.read().decode('gbk')
     #获取个人文字简介
     def getBrief(self,page):
-        pattern = re.compile('<div class="mm-aixiu-content".*?>(.*?)<!–',re.S)
+        pattern = re.compile('.*?<div.class="mm-aixiu-content".*?>(.*?)<!–',re.S)
         result = re.search(pattern,page)
-        return self.tool.replace(result.group(1))
+        if result is not None:
+            return self.tool.replace(result.group(0))
+        else:
+            print 'getBrief()==empty'
+            return 'empty'
     #获取页面所有图片
     def getAllImg(self,page):
-        pattern = re.compile('<div class="mm-aixiu-content".*?>(.*?)<!–',re.S)
+        pattern = re.compile('.*?<div class="mm-aixiu-content".*?>(.*?)<!–',re.S)
         #个人信息页面所有代码
-        content = re.search(pattern,page)
+        content = re.findall(pattern,page)
         #从代码中提取图片
-        patternImg = re.compile('<img.*?src="(.*?)"',re.S)
-        images = re.findall(patternImg,content.group(1))
+        patternImg = re.compile('<img.*?src="(.*?)">',re.S)
+        images = re.findall(patternImg,content[0])
         return images
     #保存多张写真图片
     def saveImgs(self,images,name):
@@ -100,25 +105,27 @@ class Spider:
         #获取第一页淘宝MM列表
         contents = self.getContents(pageIndex)
         for item in contents:
-            #item[0]个人详情URL,item[1]头像URL,item[2]姓名,item[3]年龄,item[4]居住地
-            print u"发现一位模特,名字叫",item[2],u"芳龄",item[3],u",她在",item[4]
-            print u"正在偷偷地保存",item[2],"的信息"
+            #item[0]个人详情URL,item[1]头像URL,item[2]是个人信息地址，item[3]姓名,item[4]年龄,item[5]居住地
+            print u"发现一位模特,名字叫",item[3],u"芳龄",item[4],u",她在",item[5]
+            print u"正在偷偷地保存",item[3],"的信息"
             print u"又意外地发现她的个人地址是",item[0]
+            print u"名字对应的个人信息地址是",item[2]
             #个人详情页面的URL
             detailURL = item[0]
+            print 'detailURL=%s' %detailURL
             #得到个人详情页面代码
-            detailPage = self.getDetailPage(detailURL)
-            #获取个人简介
-            brief = self.getBrief(detailPage)
-            #获取所有图片列表
-            images = self.getAllImg(detailPage)
-            self.mkdir(item[2])
-            #保存个人简介
-            self.saveBrief(brief,item[2])
-            #保存头像
-            self.saveIcon(item[1],item[2])
-            #保存图片
-            self.saveImgs(images,item[2])
+#             detailPage = self.getDetailPage(detailURL)
+#             #获取个人简介
+#             brief = self.getBrief(detailPage)
+#             #获取所有图片列表
+#             images = self.getAllImg(detailPage)
+#             self.mkdir(item[2])
+#             #保存个人简介
+#             self.saveBrief(brief,item[2])
+#             #保存头像
+#             self.saveIcon(item[1],item[2])
+#             #保存图片
+#             self.saveImgs(images,item[2])
     #传入起止页码，获取MM图片
     def savePagesInfo(self,start,end):
         for i in range(start,end+1):
@@ -126,4 +133,4 @@ class Spider:
             self.savePageInfo(i)
 #传入起止页码即可，在此传入了2,10,表示抓取第2到10页的MM
 spider = Spider()
-spider.savePagesInfo(2,10)
+spider.savePagesInfo(1,10)
